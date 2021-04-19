a3fb1415df2942e87122c620e8382bcaae4e80bd
Cyrielle Istin <cycycode@gmail.com>
2021-04-19 11:38:00 +0200

    we now have the landing page with some SASS and CSS without DB for the moment, it is the next step

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,15 +1,80 @@
 // == Import npm
 import React from 'react';
 
-// == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+// == Impor
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <header>
+      <h1>the crew</h1>
+      <h2>visualize all the members of the crew <br /> and add braves sailors to it !
+      </h2>
+    </header>
+    <ul className="member-list">
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+      <li>Castor</li>
+    </ul>
+    <form>
+      <p>add a new member</p>
+      <input
+        type="text"
+        placeholder="enter the name here"
+      />
+      <button>add member</button>
+    </form>
+    <div>
+      we have now 5 sailors in the crew !
+    </div>
   </div>
 );
 
